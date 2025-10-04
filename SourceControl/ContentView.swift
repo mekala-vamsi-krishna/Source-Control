1112dbeebadb8004330c7e7a6811d3a1efe1189a
Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2025-10-04 12:03:30 +0530

    First Commit

---
+++ SourceControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControl
+//
+//  Created by Mekala Vamsi Krishna on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Vamsi Krishna")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Mekala Vamsi Krishna on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
